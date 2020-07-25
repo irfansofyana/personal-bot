3f8cbfea028d48095c4cf0c76131487974b292f9
irfansofyana <irfansofyana0305@gmail.com>
2020-07-25 20:44:16 +0700

    simple bot reply user message

--- index.js
+++ index.js
@@ -33,14 +33,10 @@ app.use((err, req, res, next) => {
 // event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
     return Promise.resolve(null);
   }
-
-  // create a echoing text message
   const echo = { type: 'text', text: event.message.text };
 
-  // use reply API
   return client.replyMessage(event.replyToken, echo);
 }
 
