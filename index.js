04ffdfa64057a8be09c2db50cfa0cca71201661b
irfansofyana <irfansofyana0305@gmail.com>
2020-07-25 18:44:14 +0700

    change config to cfg

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 const line = require('@line/bot-sdk');
 const express = require('express');
-const config = require('./config');
+const cfg = require('./config');
 
 // create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: cfg.CHANNEL_ACCESS_TOKEN,
+  channelSecret: cfg.CHANNEL_SECRET,
 };
 
 // create LINE SDK client
