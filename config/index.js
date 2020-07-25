589a00be7902434d9206e8c04d0e17ee19104cb5
irfansofyana <irfansofyana0305@gmail.com>
2020-07-25 14:26:20 +0700

    add simple line-bot

--- config/index.js
+++ config/index.js
@@ -1,3 +1,6 @@
 module.exports = {
-    PORT: process.env.PORT || 30000
+    PORT: process.env.PORT || 30000,
+    BASE_URL: process.env.BASE_URL,
+    CHANNEL_ACCESS_TOKEN: process.env.CHANNEL_ACCESS_TOKEN,
+    CHANNEL_SECRET: process.env.CHANNEL_SECRET
 };
