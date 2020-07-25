368beeb97b8b57b24d6f1805f45e54a809ae4b60
irfansofyana <irfansofyana0305@gmail.com>
2020-07-25 16:25:38 +0700

    change base url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cfg = {
 const client = new line.Client(cfg);
 const app = express();
 
-app.post('/personal-bot/callback', line.middleware(cfg), (req, res) => {
+app.post('/personalbot/callback', line.middleware(cfg), (req, res) => {
     Promise
         .all(req.body.events.map(src.handleEvent))
         .then((result) => res.json(result))
@@ -22,7 +22,7 @@ app.post('/personal-bot/callback', line.middleware(cfg), (req, res) => {
         });
 });
 
-app.get('/personal-bot', (req, res) => {
+app.get('/personalbot', (req, res) => {
     res.send('The server works!');
 });
 
