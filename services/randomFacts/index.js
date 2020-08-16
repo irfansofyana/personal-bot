ed199da4d7b2c193da9dc18888a1f234d7c61a7d
irfansofyana <irfansofyana0305@gmail.com>
2020-08-16 23:41:04 +0700

    Debugging random fact

--- services/randomFacts/index.js
+++ services/randomFacts/index.js
@@ -6,7 +6,7 @@ const todayFact = async () => {
         const url = config.RANDOM_FACT_BASE_URL.concat('today.json?language=en');
         const response = await axios.get(url);
 
-        console.log(response.text);
+        console.log(response, response.text);
         return response.text;
     } catch (err) {
         console.error(err);
