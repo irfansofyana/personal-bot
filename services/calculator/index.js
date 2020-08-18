8de572e75fc1570f630164a8786934727ee3ebfe
irfansofyana <irfansofyana0305@gmail.com>
2020-08-18 14:59:45 +0700

    add calculator service

---
+++ services/calculator/index.js
@@ -0,0 +1,20 @@
+const { number } = require('mathjs');
+const mathjs = require('mathjs');
+const config = {};
+const create = mathjs.create;
+const all = mathjs.all;
+
+const math = create(all, config);
+
+const calc = (expression) => {
+    try {
+        const result = math.evaluate(expression);
+        if (typeof result === object) return result.toString();
+
+        return result;
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+module.exports = calc;
