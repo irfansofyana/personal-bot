03cc34894f452d5367c18aa0b4e44815a3868bc5
irfansofyana <irfansofyana0305@gmail.com>
2020-08-18 15:09:32 +0700

    Fix bug

--- services/index.js
+++ services/index.js
@@ -1,6 +1,7 @@
 const config = require('../config');
 const newsapi = require('./newsAPI');
 const randomFacts = require('./randomFacts');
+const calculator = require('./calculator');
 
 module.exports = {
   newsapi,
